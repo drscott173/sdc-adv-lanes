fc0720091632c1d6c0368fe4a078d5032bdfa8dc
Scott Penberthy <scott.penberthy@gmail.com>
2017-01-18 09:56:22 -0500

    initial commit for grading

---
+++ project.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#
+## Source pathnames
+#
+
+data_path = "../CarND-Advanced-Lane-Lines/"
+cam_path = data_path+"camera_cal/"
+test_path = data_path+"test_images/"
+video1_path = data_path+"project_video.mp4"
+video2_path = data_path+"challenge_video.mp4"
+video3_path = data_path+"harder_challenge_video.mp4"
+
+
+# Compute the camera calibration matrix and distortion coefficients given a set of chessboard images.
